912e58540e656503317162000e78e3969f0045e8
Sergey Harlanov <s.v.harlanov@gmail.com>
2020-06-21 17:50:22 +0300

    Add animation to checkbox

--- js/script.js
+++ js/script.js
@@ -1,9 +1,16 @@
 let checkbox1 = document.getElementById('form__checkbox1');
 let checkbox2 = document.getElementById('form__checkbox2');
+let birth_id = "form__birth"
 
 function check() {
-    if(checkbox1.checked == true)
-        document.getElementById('form__birth').style.display = "block";
-    else if(checkbox1.checked != true || checkbox2.checked == true || checkbox2.checked != true) 
-        document.getElementById('form__birth').style.display = "none";
+    if(checkbox1.checked == true) {
+        if(checkbox2.checked == true) checkbox2.checked = false;
+        document.getElementById(birth_id).style.display = "block";
+        setTimeout(() => document.getElementById(birth_id).style.opacity = 1, 0)
+    }
+    else if(checkbox1.checked != true || checkbox2.checked == true || checkbox2.checked != true) {
+        document.getElementById(birth_id).style.display = "none";
+        setTimeout(() => document.getElementById(birth_id).style.opacity = 0, 0)
+    }
 }
+

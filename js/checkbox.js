d806a94d8d603d867999d7cab6489b85036de6ef
Sergey Harlanov <s.v.harlanov@gmail.com>
2020-08-25 17:15:58 +0300

    Style changes

--- js/checkbox.js
+++ js/checkbox.js
@@ -8,11 +8,11 @@ function check() {
     if(checkbox1.checked == true) {
         if(checkbox2.checked == true) checkbox2.checked = false;
         document.getElementById(birth_id).style.display = "block";
-        setTimeout(() => document.getElementById(birth_id).style.opacity = 1, 0)
+        setTimeout( () => document.getElementById(birth_id).style.opacity = 1, 0 )
     }
     else if(checkbox1.checked != true || checkbox2.checked == true || checkbox2.checked != true) {
         document.getElementById(birth_id).style.display = "none";
-        setTimeout(() => document.getElementById(birth_id).style.opacity = 0, 0)
+        setTimeout( () => document.getElementById(birth_id).style.opacity = 0, 0 )
     }
 }
 
